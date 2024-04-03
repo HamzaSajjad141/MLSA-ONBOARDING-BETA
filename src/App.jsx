268a9054b3835864b4538d7782efab8adb85451f
Hamza Sajjad <hamzasajjad1277@gmail.com>
2024-04-03 20:58:52 +0500

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Hamza Sajjad",
+  title: "Web Developer and Python Developer",
+  email: "Hamza.Sajjad@studentambassadors.com",
+  gitHub: "HamzaSajjad141",
+  instagram: "",
+  linkedIn: "hamzasajjad01",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,27 +16,27 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/floor.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "Wooden Floor Background";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "Hi, I'm Hamza Sajjad from Pakistan! Currently in my 7th semester pursuing a Bachelor's in Computer Science at FAST NUCES. Throughout my academic journey, I've cultivated qualities like punctuality and strong work ethics.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
+  "Web Developer",
+  "Machine Learning",
   "Inclusive design",
   "Focus group testing",
   "Mobile user interfaces",
-  "Graphic design",
+  "Game design",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "This is my Portfolio Website";
 
 const About = () => {
   return (
